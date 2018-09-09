edc25a0f3feb7d10b813bdd560a372c4572dbf98
Joshua Moore <rajosh@protonmail.com>
2018-09-09 17:58:34 -0600

    Renamed file to bypass ignore conflict

---
+++ 1.collect_data.py
@@ -0,0 +1,84 @@
+# create_training_data.py
+
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from getkeys import key_check
+import os
+
+## Convert our input keys to an array for Numpy
+## [A,W,D] boolean values
+#! TO-DO: Add more keys
+def keys_to_output(keys):
+    output = [0,0,0]
+    
+    if 'A' in keys:
+        output[0] = 1
+    elif 'D' in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
+    return output
+
+training_id = '' # The label for the training files.
+starting_value = 1 # The index to start naming training files with.
+file_name = 'training_data-{}-{}.npy'.format(training_id, starting_value)
+training_data = []
+
+
+while(True): # Check if the current training files exist and make a new start value.
+    if os.path.isfile(file_name):
+        print("File exists, increasing start!")
+        starting_value += 1
+        file_name = 'training_data-{}-{}.npy'.format(training_id, starting_value)
+    else:
+        break
+
+
+def main():
+
+    print("\n\nStarting in 5")
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+    paused = False
+    
+    while(True):
+
+        if not paused:
+            # 800x600 windowed mode
+            screen = grab_screen(region=(0,40,800,640)) # Take a screenshot
+            last_time = time.time()
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            screen = cv2.resize(screen, (160,120)) # Resize the screenshot to something more reasonable for a NN
+
+            keys = key_check()
+            output = keys_to_output(keys)
+
+            training_data.append([screen,output])
+            
+            if len(training_data) % 100 == 0:
+                print(len(training_data))
+
+            if len(training_data) % 1000 == 0:
+                print(len(training_data))
+                np.save(file_name,training_data)
+                starting_value += 1
+                training_data = []
+                file_name = 'training_data-{}-{}.npy'.format(training_id, starting_value)
+
+        keys = key_check()
+        if 'T' in keys:
+            if paused:
+                paused = False
+                print('unpaused!')
+                time.sleep(1)
+            else:
+                print('Pausing!')
+                paused = True
+                time.sleep(1)
+
+
+main()
